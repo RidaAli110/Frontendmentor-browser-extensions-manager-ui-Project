eef2190635cfefeccd5831c23df512d0e038e540
RidaAli110 <rida.webdev110@gmail.com>
2026-08-18 20:34:13 +0100

    Completed Project

--- script.js
+++ script.js
@@ -3,21 +3,26 @@
 const cardList = document.querySelector('.card-list');
 const filterBtns = document.querySelectorAll('.filter-btn');
 const filterContainer = document.getElementById('filter');
+const themeIcon = document.querySelector('#theme-btn img');
+const appLogo = document.querySelector('#app-logo');
 
+// The array that holds the extensions
 let extensions = [];
 
 // Fetching from JSON file
 async function getExtensions() {
   const response = await fetch('./data.json');
+  // putting the JSON data in the extensions array
   extensions = await response.json();
 
+  // The Extensions get put into the card
   createCard(extensions);
 }
 getExtensions();
 
 //Create card
-function createCard(array) {
-  array.forEach((extension) => {
+function createCard(arrayOfEachExtension) {
+  arrayOfEachExtension.forEach((extension) => {
     const cardLi = document.createElement('li');
     cardLi.classList.add('card');
     cardLi.innerHTML = `<div class="details-container">
@@ -33,9 +38,9 @@ function createCard(array) {
               </div>
             </div>
             <div class="btn-container">
-              <button class="remove-btn">Remove</button>
+              <button type='button' class="remove-btn" data-name='${extension.name}'>Remove</button>
               <label class="switch">
-                  <input type="checkbox" ${extension.isActive ? 'checked' : ''}/>
+                  <input aria-label='${extension.name}' type="checkbox" data-name='${extension.name}'  ${extension.isActive ? 'checked' : ''}/>
                 <span class="slider round"></span>
               </label>
             </div>`;
@@ -43,6 +48,41 @@ function createCard(array) {
     // Add To Dom
     cardList.appendChild(cardLi);
   });
+}
+
+// Theme button
+function toggleTheme() {
+  document.body.classList.toggle('light-mode');
+  if (document.body.classList.contains('light-mode')) {
+    themeIcon.src = './assets/images/icon-moon.svg';
+    appLogo.src = './assets/images/logo.svg';
+  } else {
+    themeIcon.src = './assets/images/icon-sun.svg';
+    appLogo.src = './assets/images/logo-dark.svg';
+  }
+}
+
+// Remove Button
+function removeExtension(e) {
+  if (e.target.classList.contains('remove-btn')) {
+    const name = e.target.dataset.name;
+    const updateExtensions = extensions.filter((extension) => {
+      return extension.name !== name;
+    });
+    extensions = updateExtensions;
+    e.target.closest('.card').remove();
+  }
+}
+
+// Toggle button to change if extension button is active or not
+function toggleExtension(e) {
+  if (e.target.type === 'checkbox') {
+    const name = e.target.dataset.name;
+    const extension = extensions.find((extension) => {
+      return extension.name === name;
+    });
+    extension.isActive = !extension.isActive;
+  }
 }
 
 // Clear Cards from filters
@@ -60,6 +100,7 @@ function filterBtnColor(e) {
   }
 }
 
+// Filter Extension pages
 function filterExtensions(e) {
   if (e.target.classList.contains('filter-btn')) {
     clearCard();
@@ -80,7 +121,6 @@ function filterExtensions(e) {
         break;
 
       default:
-        console.log('default is running');
         break;
     }
   }
@@ -89,3 +129,6 @@ function filterExtensions(e) {
 // Event Listeners
 filterContainer.addEventListener('click', filterExtensions);
 filterContainer.addEventListener('click', filterBtnColor);
+cardList.addEventListener('change', toggleExtension);
+cardList.addEventListener('click', removeExtension);
+themeIcon.addEventListener('click', toggleTheme);
